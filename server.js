dbbb96eba0a1ec3478e00f4270e39a2534014a9c
BarbosaLuis <barbosaluis01@gmail.com>
2016-12-20 11:45:22 +0000

    recebe arquivos teoricamente

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+	fs      = require('fs'),
+	app     = express(),
+	path	= require('path'),
+//	eps     = require('ejs'),
+//	morgan  = require('morgan'),
+	busboy  = require('busboy');
+	
+//Object.assign=require('object-assign')
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+//app.engine('html', require('ejs').renderFile);
+//app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+	destino = process.env.OPENSHIFT_DATA_DIR || './destino';
 
 
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   res.end("HELLO WORLD!!!");
+});
+
+app.post('/upload', function(req, res){
+  var busboy = new busboy({headers: req.headers});
+	
+	busboy.on('file', function(fieldname, file, filename, encoding, mimetype){
+		var save = path.join(destino, path.basename(fieldname));
+		file.pipe(fs.createWriteStream(save));
+	});
+
+	busboy.on('finish', function(){
+		res.writeHead(200, {'Response': 'Upload com sucesso'});
+		res.end("The cake is a lie!");
+	});
+	return req.pipe(busboy);
 });
 
 
